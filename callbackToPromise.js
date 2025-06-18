5d8344e644eb4f931d2dd58653489078d2e7f756
Raul Cosio <raulcosio934@gmail.com>
2025-06-18 09:39:42 -0600

    Update callbackToPromise.js

--- callbackToPromise.js
+++ callbackToPromise.js
@@ -5,3 +5,19 @@ export function obtenerDatosPromise(callback) {
         callback(null, { data: 'datos importantes' });
     }, 2000);
 }
+
+// Transformay la siguiente función para que funcione con promesas en lugar de callbacks:
+
+export function obtenerDatosPromise(callback) {
+    setTimeout(() => {
+        callback(null, { data: 'datos importantes' });
+    }, 2000);
+}
+
+export function obtenerDatosPromise() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ data: 'datos importantes' });
+        }, 2000);
+    });
+}
